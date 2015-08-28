7c5b39837242acb7c91871e57a124029f41f527d
Hakam El Assouad <hakamelassouad@gmail.com>
2015-08-28 16:07:20 +0200

    mocha: update ramdisk

--- mocha.mk
+++ mocha.mk
@@ -17,7 +17,18 @@ DEVICE_PACKAGE_OVERLAYS += device/xiaomi/mocha/overlay
 $(call inherit-product,  $(SRC_TARGET_DIR)/product/full_base.mk)
 
 PRODUCT_PACKAGES += \
-    init.none.rc
+    init.none.rc \
+    fstab.mocha \
+    init.mocha.rc \
+    init.t124_emmc.rc \
+    init.t124_sata.rc \
+    init.cal.rc \
+    init.hdcp.rc \
+    init.t124.rc \
+    init.tlk.rc \
+    init.nv_dev_board.usb.rc \
+    power.mocha.rc \
+    ueventd.mocha.rc	
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=1417730315
 PRODUCT_NAME := full_mocha
